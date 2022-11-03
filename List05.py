d939e6ed462f90c5e8f9eb9a8815d125f67db8b2
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-11-04 00:08:25 +0500

    5 misol

--- List05.py
+++ List05.py
@@ -1,10 +1,12 @@
 def main(list1,i):
     """
-    A list of several elements is given. i Return the item in the index.
+    A list of several elements is given. x
     Args:
         list1 (list): parameter
         i (int): parameter
     Returns:
         list: return answer
     """
-    return
+    x=list1[i]
+    return x
+print(main([1, 't', 3, 4, 5],1))
