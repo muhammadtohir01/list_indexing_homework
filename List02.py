f8009f5ee26dfe7cf97bb4d64f6c05fad064bcbd
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-11-04 00:03:20 +0500

    2mmmm

--- List02.py
+++ List02.py
@@ -6,7 +6,5 @@ def main(list1):
     Returns:
         list: return answer
     """
-    #list1=[1,2,3,4,5,6]
-    d=list1[:1]
-    return d
-print(main([1,2,3,4,5]))
+    return list1[0]
+print(main(['y',2,3,4,5]))
